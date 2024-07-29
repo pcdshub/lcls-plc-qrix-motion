49e12b0dd34edd17d20ae3fe32591c7f37f3c604
Nicholas Lentz <nlentz@slac.stanford.edu>
2024-07-29 14:39:13 -0700

    Increased maximum speed for YDF motors. Added PVs and variables for reseting and viewing circuit breaker status for ethercat circuit breakers on the spectrometer arm.

--- iocBoot/ioc-qrix-motion/st.cmd
+++ iocBoot/ioc-qrix-motion/st.cmd
@@ -44,7 +44,7 @@ epicsEnvSet("ASYN_PORT",        "ASYN_PLC")
 epicsEnvSet("IPADDR",           "172.21.140.101")
 epicsEnvSet("AMSID",            "172.21.140.101.1.1")
 epicsEnvSet("AMS_PORT",         "851")
-epicsEnvSet("ADS_MAX_PARAMS",   "6349")
+epicsEnvSet("ADS_MAX_PARAMS",   "6373")
 epicsEnvSet("ADS_SAMPLE_MS",    "50")
 epicsEnvSet("ADS_MAX_DELAY_MS", "100")
 epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
@@ -884,8 +884,8 @@ cd "$(IOC_TOP)"
 ## PLC Project Database files ##
 dbLoadRecords("qrix_motion.db", "PORT=$(ASYN_PORT),PREFIX=PLC:QRIX:MOT:,IOCNAME=$(IOC),IOC=$(IOC)")
 
-# Total records: 5349
-callbackSetQueueSize(12698)
+# Total records: 5373
+callbackSetQueueSize(12746)
 
 # Autosave and archive settings:
 save_restoreSet_status_prefix("PLC:QRIX:MOT:")
