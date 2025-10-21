8173e20e293ebe6183243341d7b450c567ba5f8e
Josue Zamudio <jozamudi@slac.stanford.edu>
2025-10-21 11:35:06 -0700

    build: ioc

--- iocBoot/ioc-qrix-motion/st.cmd
+++ iocBoot/ioc-qrix-motion/st.cmd
@@ -1,11 +1,11 @@
-#!c:/Repos/ads-ioc/R0.8.0///bin/rhel7-x86_64/adsIoc
+#!/reg/g/pcds/epics/ioc/common/ads-ioc/R0.8.0/bin/rhel7-x86_64/adsIoc
 ################### AUTO-GENERATED DO NOT EDIT ###################
 #
 #         Project: lcls-plc-qrix-motion.tsproj
 #        PLC name: qrix_motion (qrix_motion Instance)
 # Generated using: pytmc 2.18.2
-# Project version: unknown
-#    Project hash: unknown
+# Project version: a932263
+#    Project hash: a932263b99db2ea35bb5019b5020197d8215db87
 #     PLC IP/host: 172.21.140.101
 #      PLC Net ID: 172.21.140.101.1.1
 #  ** Production mode IOC **
@@ -29,7 +29,7 @@
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "" )
+epicsEnvSet("ENGINEER", "jozamudi" )
 epicsEnvSet("LOCATION", "PLC:QRIX:MOT" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
@@ -52,7 +52,7 @@ epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
 epicsEnvSet("ADS_TIME_SOURCE",  "0")
 
 # Add a route to the PLC automatically:
-system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.140.101 ^172.*$")
+system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.140.101 ^172.*")
 
 # adsAsynPortDriverConfigure(portName, ipaddr, amsaddr, amsport,
 #    asynParamTableSize, priority, noAutoConnect, defaultSampleTimeMS,
@@ -859,7 +859,7 @@ dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
 dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:QRIX:MOT,IDX=1,TASK_PORT=350")
 dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:QRIX:MOT")
 
-dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:QRIX:MOT,PROJECT=lcls-plc-qrix-motion.tsproj,HASH=unknown,VERSION=unknown,PYTMC=2.18.2,PLC_HOST=172.21.140.101")
+dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:QRIX:MOT,PROJECT=lcls-plc-qrix-motion.tsproj,HASH=a932263,VERSION=a932263,PYTMC=2.18.2,PLC_HOST=172.21.140.101")
 
 #   LCLS General: * -> 2.10.0 (SLAC)
 dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:QRIX:MOT,DEPENDENCY=LCLS_General,VERSION=2.10.0,VENDOR=SLAC")
